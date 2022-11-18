cf40fc9cf91109a857255dd55d5129cd525b5972
Julaan <jumarina@gmail.com>
2022-11-18 18:14:32 +0100

    Style purple

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 function calculateTip(event) {
-
+    event.preventDefault();
+    console.log(event);    
 }
 
 document.getElementById("totalTip").style.display= "none";
 document.getElementById("eachPerson").style.display= "none";
+
+document.getElementById("tipsForm").addEventListener('submit', calculateTip);
